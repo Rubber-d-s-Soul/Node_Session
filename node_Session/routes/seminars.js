f104ecccb7e41a1542e28efd2adf68520bd190a2
杉山大輔 <rubber.d.s.soul@gmail.com>
2017-10-30 12:51:21 +0900

    header,sidebarをコンポーネント化、seminars.ejsを変更

--- node_Session/routes/seminars.js
+++ node_Session/routes/seminars.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('seminars', { title: '勉強会一覧ページ' });
+    res.render('seminars', { title: '一覧ページ' });
 });
 
 module.exports = router;
